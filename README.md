87666f0e66c215c622e7756774e364fd4e32d1e2
Charlie Parrott <cparrott5700@gmail.com>
2021-04-25 13:08:18 -0400

    Homework Javascript quiz

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Javascript-Quiz
+
+The following is a Javascript quiz that tracks a users responses and renders a score.
+
+
+I was able to render the questions and track the correct answers. The timer is a difficult feature that I could not figure out how to track and subtract time for wrong answers. 
--- script.js
+++ script.js
@@ -1,78 +1,89 @@
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
+var pos = 0;
+var correct = 0;
+var test, test_status, question, choice, choices, chA, chB, chC, chD;
 
-function buildQuiz(){
-    // variable to store the HTML output
-    const output = [];
+var questions = [
+  {
+    question: "Who invented Javascript?",
+    a: "Douglas Crockford",
+    b: "Brendan Eich",
+    c: "Sheryl Sandberg",
+    d: "Einstein",
+    answer: "B"
+  },
+  {
+    question: "Which one of these is a Javascript package manager?",
+    a: "Typescript",
+    b: "Node.js",
+    c: "npm",
+    d: "Bootstrap",
+    answer: "C"
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    a: "jQuery",
+    b: "RequireJS",
+    c: "Angular",
+    d: "ESLint",
+    answer: "D"
+  },
+  {
+    question: "Where is the correct place to insert a JavaScript?",
+    a: "The head section",
+    b: "Both the head section and the body section are correct",
+    c: "The body section",
+    d: "The footer",
+    answer: "B"
+  }
+];
+
+function get(x){
+  return document.getElementById(x);
+}
+
+  function renderQuestion(){
+    test = get("test");
+    if(pos >= questions.length){
+      test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
+      get("test_status").innerHTML = "Test completed";
+      pos = 0;
+      correct = 0;
+      return false;
+    }
+
+    get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
   
-    // for each question...
-    myQuestions.forEach(
-      (currentQuestion, questionNumber) => {
-  
-        // variable to store the list of possible answers
-        const answers = [];
-  
-        // and for each available answer...
-        for(letter in currentQuestion.answers){
-  
-          // ...add an HTML radio button
-          answers.push(
-            `<label>
-              <input type="radio" name="question${questionNumber}" value="${letter}">
-              ${letter} :
-              ${currentQuestion.answers[letter]}
-            </label>`
-          );
-        }
-  
-        // add this question and its answers to the output
-        output.push(
-          `<div class="question"> ${currentQuestion.question} </div>
-          <div class="answers"> ${answers.join('')} </div>`
-        );
-      }
-    );
-  
-    // finally combine our output list into one string of HTML and put it on the page
-    quizContainer.innerHTML = output.join('');
+    question = questions[pos].question;
+    chA = questions[pos].a;
+    chB = questions[pos].b;
+    chC = questions[pos].c;
+    chD = questions[pos].d;
+
+    test.innerHTML = "<h3>"+question+"</h3>";
+
+    test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
+    test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
+    test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br>";
+    test.innerHTML += "<label> <input type='radio' name='choices' value='D'> "+chD+"</label><br>";
+    test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
   }
 
-function showResults(){}
-
-// display quiz right away
-buildQuiz();
-
-// on submit, show results
-submitButton.addEventListener('click', showResults);
-
-const myQuestions = [
-    {
-      question: "Who invented JavaScript?",
-      answers: {
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Which one of these is a JavaScript package manager?",
-      answers: {
-        a: "Node.js",
-        b: "TypeScript",
-        c: "npm"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Which tool can you use to ensure code quality?",
-      answers: {
-        a: "Angular",
-        b: "jQuery",
-        c: "RequireJS",
-        d: "ESLint"
-      },
-      correctAnswer: "d"
+  function checkAnswer(){
+   choices = document.getElementsByName("choices");
+    for(var i=0; i<choices.length; i++){
+      if(choices[i].checked){
+    choice = choices[i].value;
     }
-  ];
+  }
+
+    if(choice == questions[pos].answer){
+      correct++;
+    }
+
+  pos++;
+
+  renderQuestion();
+}
+
+window.addEventListener("load", renderQuestion);
+
